1a3b7c3ff0bb0ff5412dd810d4e72949ae6c8b26
Fábio <40219014+SiddSparrow@users.noreply.github.com>
2026-05-15 17:06:28 -0300

    feat: implementação das factories nos models

--- app/Models/Documento.php
+++ app/Models/Documento.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Documento extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['user_id', 'cpf', 'rg'];
 
     public function user()
