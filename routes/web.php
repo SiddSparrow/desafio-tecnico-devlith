0bb4b5021c6e46928b938b915a4b4eb0b2a3eb88
Fábio <40219014+SiddSparrow@users.noreply.github.com>
2026-05-15 21:23:51 -0300

    feat: ajustes para excel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExportacaoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/exportacao/download', [ExportacaoController::class, 'download'])
+    ->name('exportacao.download');
